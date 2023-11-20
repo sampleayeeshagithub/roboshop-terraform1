b36777ddcf6c468671d87ae18b647376d9edf541
Mulla Ayeesha <ayeeshamulla0708@gmailcom>
2023-11-20 15:13:51 +0530

    Ansible

---
+++ env-dev/state.tfvars
@@ -0,0 +1,3 @@
+bucket = "terraform-a75"
+key    = "roboshop-project/dev/terraform.tfstate"
+region = "us-east-1"
--- main.tf
+++ main.tf
@@ -1,57 +1,14 @@
-variable "components" {
-  default = [
-    "frontend",
-    "cart",
-    "catalogue",
-    "user",
-    "payment",
-    "shipping",
-    "dispatch",
-    "mysql",
-    "rabbitmq",
-    "redis",
-    "mongodb",
-  ]
-}
-
-
-data "aws_ami" "ami" {
-  most_recent = true
-  name_regex  = "Centos-8-DevOps-Practice"
-  owners      = ["973714476881"]
-}
-
-
-
-resource "aws_instance" "instance" {
-  count = length(var.components)
-  ami = data.aws_ami.ami.id
-  instance_type = "t3.micro"
-  vpc_security_group_ids = ["sg-00493eacf9da2075a"]
-  tags = {
-    Name = element(var.components, count.index)
-  }
-}
-
-resource "aws_route53_record" "record" {
-  count = length(var.components)
-  zone_id = "Z0073724138YO075ETFE9"
-  name    = "${element(var.components, count.index)}-dev"
-  type    = "A"
-  ttl     = 30
-  records = [element(aws_instance.instance.*.private_ip, count.index)]
-}
-
-resource "null_resource" "set-hostname" {
-  count = length(var.components)
-  provisioner "remote-exec" {
-    connection {
-      host = element(aws_instance.instance.*.private_ip, count.index)
-      user = "root"
-      password = "DevOps321"
-    }
-    inline = [
-      "set-hostname -skip-apply ${var.components[count.index]}"
-    ]
-  }
+module "vpc" {
+  source                 = "git::https://github.com/sampleayeeshagithub/tf-module-vpc.git"
+  vpc_cidr               = var.vpc_cidr
+  env                    = var.env
+  public_subnets         = var.public_subnets
+  web_subnets            = var.web_subnets
+  app_subnets            = var.app_subnets
+  db_subnets             = var.db_subnets
+  azs                    = var.azs
+  account_no             = var.account_no
+  default_vpc_id         = var.default_vpc_id
+  default_vpc_cidr       = var.default_vpc_cidr
+  default_route_table_id = var.default_route_table_id
 }
