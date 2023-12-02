acaac57dd60e1654cb61822a76b8543f2cee9029
Mulla Ayeesha <ayeeshamulla0708@gmailcom>
2023-12-02 17:38:20 +0530

    Ansible

--- main.tf
+++ main.tf
@@ -59,6 +59,7 @@ module "rabbitmq" {
   vpc_id                 = module.vpc.vpc_id
   kms_key_id             = var.kms_key_id
   rabbitmq_instance_type = var.rabbitmq_instance_type
+  zone_id                = "Z0073724138YO075ETFE9"
 }
 
 module "ms-components" {
